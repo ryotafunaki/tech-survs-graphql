477236b699ec7114ea54fc2cac6457914ca96c56
Ryota Funaki <ryota.funaki@ngv.jp>
2024-08-05 01:11:01 +0000

    Initial commit

---
+++ src/common/node-redis/node-redis.factory.ts
@@ -0,0 +1,25 @@
+// Copyright (c) 2024 RFull Development
+// This source code is managed under the MIT license. See LICENSE in the project root.
+import { FactoryProvider } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { createClient } from 'redis'
+
+type NodeRedisClientType = ReturnType<typeof createClient>
+const nodeRedisClientFactory: FactoryProvider<NodeRedisClientType> = {
+  provide: 'NODE_REDIS',
+  inject: [ConfigService],
+  useFactory: (configService: ConfigService) => {
+    const config = configService.get('cache')
+    const client = createClient({
+      socket: {
+        host: config.host,
+        port: config.port,
+      },
+    })
+    client.connect()
+    console.log('Redis client created')
+    return client
+  },
+}
+export { NodeRedisClientType }
+export default nodeRedisClientFactory
